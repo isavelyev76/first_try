1f55fb34a6ebd74cd5f2ecffc470ef8cf9a2e104
isavelyev76 <154694236+isavelyev76@users.noreply.github.com>
2023-12-23 22:42:50 +0300

    "Добавлены тестовые данные"

--- example of project/main.cpp
+++ example of project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #10. Product Catalog\n";
     cout << "Author: Ilya Savelyev\n";
+    cout << "Group: 23PInzh1d";
     return 0;
 }
